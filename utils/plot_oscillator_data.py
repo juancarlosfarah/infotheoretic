4caf164b86a956e588ebc51ffa886dba049626a9
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-06-17 14:41:59 +0100

    Can specify query for data to plot. Improve labels.

--- utils/plot_oscillator_data.py
+++ utils/plot_oscillator_data.py
@@ -4,6 +4,7 @@ import pymongo
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.interpolate as spi
+from copy import deepcopy
 
 
 def connect(database):
@@ -49,15 +50,39 @@ def plot_one(threshold):
 def plot(phi="integrated_information_e",
          save=False,
          path="$HOME",
-         ext="svg"):
+         ext="svg",
+         query=None):
     db = connect("individual_project")
 
+    duration = "Various"
+
+    if "duration" in query:
+        duration = query['duration']
+
+    if not query:
+        query = dict()
+
+    q1 = deepcopy(query)
+    q1['threshold'] = 0.9
+
+    q2 = deepcopy(query)
+    q2['threshold'] = 0.8
+
+    q3 = deepcopy(query)
+    q3['threshold'] = 0.7
+
+    q4 = deepcopy(query)
+    q4['threshold'] = 0.6
+
+    q5 = deepcopy(query)
+    q5['threshold'] = 0.5
+
     cursors = {
-        "0.9": db.oscillator_simulation.find({"threshold": 0.9}),
-        "0.8": db.oscillator_simulation.find({"threshold": 0.8}),
-        "0.7": db.oscillator_simulation.find({"threshold": 0.7}),
-        "0.6": db.oscillator_simulation.find({"threshold": 0.6}),
-        "0.5": db.oscillator_simulation.find({"threshold": 0.5})
+        "0.9": db.oscillator_simulation.find(q1),
+        "0.8": db.oscillator_simulation.find(q2),
+        "0.7": db.oscillator_simulation.find(q3),
+        "0.6": db.oscillator_simulation.find(q4),
+        "0.5": db.oscillator_simulation.find(q5)
     }
 
     beta = dict()
@@ -100,7 +125,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Global Synchrony")
         plt.ylabel("Integrated Information Empirical")
-        plt.title("Integrated Information Empirical over Global Synchrony")
+        plt.title("Integrated Information Empirical vs Global Synchrony\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(global_sync[key],
                                    integrated_information[key],
                                    color=colors[key],
@@ -119,7 +145,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Beta")
         plt.ylabel("Integrated Information Empirical")
-        plt.title("Integrated Information Empirical over Beta")
+        plt.title("Integrated Information Empirical vs Beta\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(beta[key],
                                    integrated_information[key],
                                    color=colors[key],
@@ -138,7 +165,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Beta")
         plt.ylabel("Chi")
-        plt.title("Chi over Beta")
+        plt.title("Chi vs Beta\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(beta[key],
                                    chi[key],
                                    color=colors[key],
@@ -157,7 +185,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Beta")
         plt.ylabel("Lambda")
-        plt.title("Lambda over Beta")
+        plt.title("Lambda vs Beta\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(beta[key],
                                    lamda[key],
                                    color=colors[key],
@@ -176,7 +205,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Chi")
         plt.ylabel("Integrated Information Empirical")
-        plt.title("Integrated Information Empirical over Chi")
+        plt.title("Integrated Information Empirical vs Chi\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(chi[key],
                                    integrated_information[key],
                                    color=colors[key],
@@ -195,7 +225,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Lambda")
         plt.ylabel("Integrated Information Empirical")
-        plt.title("Integrated Information Empirical over Lambda")
+        plt.title("Integrated Information Empirical vs Lambda\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(lamda[key],
                                    integrated_information[key],
                                    color=colors[key],
@@ -214,7 +245,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Beta")
         plt.ylabel("Coalition Entropy")
-        plt.title("Coalition Entropy over Beta")
+        plt.title("Coalition Entropy vs Beta\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(beta[key],
                                    coalition_entropy[key],
                                    color=colors[key],
@@ -233,7 +265,8 @@ def plot(phi="integrated_information_e",
         labels.append(key)
         plt.xlabel("Coalition Entropy")
         plt.ylabel("Integrated Information Empirical")
-        plt.title("Integrated Information Empirical over Coalition Entropy")
+        plt.title("Integrated Information Empirical vs Coalition Entropy\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(coalition_entropy[key],
                                    integrated_information[key],
                                    color=colors[key],
@@ -253,7 +286,8 @@ def plot(phi="integrated_information_e",
         plt.xlabel("Integrated Information Empirical Tilde")
         plt.ylabel("Integrated Information Empirical")
         plt.title("Integrated Information Empirical vs "
-                  "Integrated Information Empirical Tilde")
+                  "Integrated Information Empirical Tilde\n"
+                  "Duration = " + str(duration) + ", Tau = 1")
         handles.append(plt.scatter(phi_e_tilde[key],
                                    integrated_information[key],
                                    color=colors[key],
@@ -450,12 +484,18 @@ def plot_curves():
 
 
 if __name__ == "__main__":
+    q = {
+        "duration": 5000
+    }
+
     plot(phi="integrated_information_e",
          save=True,
          path="/Users/juancarlosfarah/Git/infotheoretic/docs/phi_e/",
-         ext="svg")
+         ext="svg",
+         query=q)
     plot(phi='integrated_information_e_tilde',
          save=True,
          path="/Users/juancarlosfarah/Git/infotheoretic/docs/phi_e_tilde/",
-         ext="svg")
+         ext="svg",
+         query=q)
     # plot_curves()
