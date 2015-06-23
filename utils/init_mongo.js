911b943252088c006744136d6d915e4aa5ecf66f
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-06-23 23:40:09 +0100

    Create JS functions to remove simulation data, create indexes and duplicate entries.

--- utils/init_mongo.js
+++ utils/init_mongo.js
@@ -4,17 +4,36 @@
 
 // Create Indexes
 // ==============
-var db = db.getSiblingDB("infotheoretic");
-db.oscillator_data.ensureIndex({ "simulation_id": -1, "_id": 1 });
-
-// Remove Surrogate Data
-// =====================
-var c = db.oscillator_simulation.find({"is_surrogate": true});
-
-while (c.hasNext()) {
-    var doc = c.next();
-    var sim_id = doc['_id'];
-    db.oscillator_data.remove({"simulation_id": sim_id});
+function createIndexes() {
+    var db = db.getSiblingDB("infotheoretic");
+    db.oscillator_data.ensureIndex({ "simulation_id": -1, "_id": 1 });
 }
 
-db.oscillator_simulation.remove({"is_surrogate": true});
+// Remove Simulation Data
+// =======================
+function removeSimulationData(key) {
+    var q = {};
+    q[key] = true;
+    var c = db.oscillator_simulation.find(q);
+
+    while (c.hasNext()) {
+        var doc = c.next();
+        var sim_id = doc['_id'];
+        db.oscillator_data.remove({"simulation_id": sim_id});
+    }
+
+    db.oscillator_simulation.remove(q);
+
+}
+
+// Copy Data
+// =========
+function duplicate() {
+    var c = db.oscillator_simulation.find({"is_surrogate": true}, {"_id": 0});
+
+    while (c.hasNext()) {
+        var doc = c.next();
+        doc["is_sorted"] = true;
+        db.oscillator_data.insert(doc);
+    }
+}
