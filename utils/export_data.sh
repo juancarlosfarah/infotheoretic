d7e84bc67e6f201c1e8c002cb11f7509ffdc3eef
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-10-07 09:11:38 +0100

    Edit export script.

--- utils/export_data.sh
+++ utils/export_data.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-mongoexport -d infotheoretic -c kuramoto_simulation --fieldFile fields.txt --type=csv --out kuramoto_gamma_5.csv -q '{ threshold: 0.5 }'
-mongoexport -d infotheoretic -c kuramoto_simulation --fieldFile fields.txt --type=csv --out kuramoto_gamma_6.csv -q '{ threshold: 0.6 }'
-mongoexport -d infotheoretic -c kuramoto_simulation --fieldFile fields.txt --type=csv --out kuramoto_gamma_7.csv -q '{ threshold: 0.7 }'
-mongoexport -d infotheoretic -c kuramoto_simulation --fieldFile fields.txt --type=csv --out kuramoto_gamma_8.csv -q '{ threshold: 0.8 }'
-mongoexport -d infotheoretic -c kuramoto_simulation --fieldFile fields.txt --type=csv --out kuramoto_gamma_9.csv -q '{ threshold: 0.9 }'
+mongoexport -d infotheoretic -c k_simulation --fieldFile fields.txt --type=csv --out kuramoto.csv -q '{ "beta": { "$lt": 1 } }'
